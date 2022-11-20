fe5274dae49c236cf2e88dc4ba40d63da5821222
MateuszPestka <pes.matthew@gmail.com>
2022-11-20 22:16:43 +0000

    Handling events

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import css from './App.module.css';
 import Sidebar from './components/Sidebar';
 import NavBarSimple from './components/NavBarSimple';
+import NavBarForms from './components/NavBarForms';
 
 function App() {
   return(
     <div className={css.App}>
       <Sidebar />
-      <NavBarSimple />
+      {/* <NavBarSimple /> */}
+      <NavBarForms />
     </div>
   );
 }
